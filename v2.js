4f4aa31e75dba3557fc5d552eeb51758b138a955
Phodal Huang <h@phodal.com>
2017-10-23 08:01:05 -0500

    Create v2.js

---
+++ v2.js
@@ -0,0 +1,12 @@
+const myCode = {
+  isFinished: true
+};
+let marry = () => {};
+let you = () => {} ;
+let meet = () => {} ;
+let yourLove = () => {} ;
+let action = '分享到朋友圈';
+let when = () => {};
+
+when(myCode.isFinished).then(marry(you));
+when(action === '分享到朋友圈').then(meet(yourLove));
